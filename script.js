f1ed4b0910290ecb69d25d7c4112221d09829053
atef <mail@aamri.de>
2021-07-05 14:48:03 +0200

    #70-Toggle Background Color

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 // Log "Welcome to Coulette" to the console in your script file
 console.log("Wellcome to Coulette!");
+// Skript-p48 addEventListner()
+
+//make var: header with "header" inside and search for it
+let header = document.querySelector("header");
+console.log(header);
+
+// JS Code to change header Color
+function changeColor() {
+	let header = document.querySelector("header");
+	let headerBg = header.style.backgroundColor;
+	if (headerBg === "turquoise") {
+		header.style.backgroundColor = "white";
+	} else {
+		header.style.background = "turquoise";
+	}
+}
+
+// search btn in my document
+let btn = document.querySelector("button");
+btn.addEventListener("click", changeColor);
