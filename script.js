68c8d5ef683dbe9d082adab024feae5dc70514f7
atef <mail@aamri.de>
2021-07-05 23:30:34 +0200

    #71-Random Color

--- script.js
+++ script.js
@@ -2,21 +2,52 @@
 console.log("Wellcome to Coulette!");
 // Skript-p48 addEventListner()
 
-//make var: header with "header" inside and search for it
-let header = document.querySelector("header");
-console.log(header);
-
+changeColor();
 // JS Code to change header Color
 function changeColor() {
 	let header = document.querySelector("header");
-	let headerBg = header.style.backgroundColor;
-	if (headerBg === "turquoise") {
-		header.style.backgroundColor = "white";
-	} else {
-		header.style.background = "turquoise";
-	}
+	header.style.backgroundColor = randomHexColor();
+	//Suche nach dem DOM-Element #colorValue und schreibe das Ergebnis in blub
+	let blub = document.querySelector("#colorValue");
+	//BackgroundColor-Wert im DOM-Element wiedergeben
+	blub.innerText = header.style.backgroundColor;
 }
 
 // search btn in my document
 let btn = document.querySelector("button");
 btn.addEventListener("click", changeColor);
+
+//::::::::: Given Code :::::::::::
+
+/**
+ * Toggle color of header
+ * Generate random number between min and max
+ */
+function randomNumber(min, max) {
+	const num = Math.random() * (max - min + 1) + min;
+	return Math.floor(num);
+}
+
+/**
+ * Generate random hex number for color
+ */
+function randomHexNumber() {
+	let hex = randomNumber(0, 255).toString(16);
+	if (hex.length === 1) {
+		hex = "0" + hex;
+	}
+	return hex;
+}
+
+/**
+ * Generate random hex color
+ */
+function randomHexColor() {
+	const red = randomHexNumber();
+	const green = randomHexNumber();
+	const blue = randomHexNumber();
+
+	return ("#" + red + green + blue).toUpperCase();
+}
+
+//::::::::.. End Given Code
